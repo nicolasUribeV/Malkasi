7d818284a29a5811fe75c95cba25022664039204
robsandova <roberto@roberto>
2014-11-25 19:10:28 -0300

    Validadores modulo 2: -Rut no puede ingresar si ya lo insertaste -Nombre de usuario no puede ser ingresado si ya esta en el sistema -Cambio de mayuscula a miniscula de rut's -k

---
+++ malkasi-web/src/main/java/validators/utils/SameUserNameValidator.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package validators.utils;
+
+import entities.Academico;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import sessionbeans.AcademicoFacadeLocal;
+
+@FacesValidator("SameUserNameValidator")
+public class SameUserNameValidator implements Validator {
+
+    @EJB
+    private AcademicoFacadeLocal ejbFacade;
+    
+    private String userName;
+
+    public SameUserNameValidator() {
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        userName = value.toString();
+        if (!validarUserName(userName)) {
+            FacesMessage msg = new FacesMessage("Nombre de usuario ya ingresado en el sistema", "Invalid Rut format.");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
+    }
+
+    public boolean validarUserName(String rut) {
+        
+        boolean validacion = false;
+        
+        try {
+            if(ejbFacade.FindWithUserName(userName).size() == 0){
+                validacion = true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return validacion;
+    }
+}
